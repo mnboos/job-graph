28ed458333123f8bc078b04ac1802c379c5de99a
Martin Boos <mboos@outlook.com>
2025-09-22 11:16:59 +0200

    build(docker): fix ssl connections with custom ssl cert

--- docker/graphhopper-entrypoint.sh
+++ docker/graphhopper-entrypoint.sh
@@ -4,13 +4,13 @@ OSM_DATA_FILE=$(basename $OSM_DATA_URL)
 echo "Requested OSM data: ${OSM_DATA_FILE}"
 OSM_DATA_FILE=/${OSM_DATA_DIR}/${OSM_DATA_FILE}
 
-if [ ! -f $OSM_DATA_FILE ]; then
+if [ ! -s $OSM_DATA_FILE ]; then
     echo "Downloading OSM data"
 
     USER_AGENT="job-graph"
     wget \
+    --no-check-certificate \
 		--user-agent="$USER_AGENT" \
-		-q \
 		--show-progress \
 		--progress=bar:force:noscroll \
 		-O $OSM_DATA_FILE \
@@ -18,12 +18,12 @@ if [ ! -f $OSM_DATA_FILE ]; then
 	
 fi
 
-if [ -f $OSM_DATA_FILE ]; then
+if [ -s $OSM_DATA_FILE ]; then
     echo "Start graphhopper"
 	
 	#java -jar graphhopper.jar import /config.yaml
 	java -jar graphhopper.jar check /config.yaml
-    java -Xmx6g -Xms6g -Ddw.graphhopper.datareader.file=${OSM_DATA_FILE} -jar graphhopper.jar server /config.yaml
+    java -Xmx12g -Xms12g -Ddw.graphhopper.datareader.file=${OSM_DATA_FILE} -jar graphhopper.jar server /config.yaml
 else
     echo "Could not start graphhopper, no OSM data was found."
 	exit 1
--- docker/graphhopper.Dockerfile
+++ docker/graphhopper.Dockerfile
@@ -7,6 +7,11 @@ WORKDIR /graphhopper
 
 ADD https://github.com/graphhopper/graphhopper/releases/download/10.2/graphhopper-web-10.2.jar graphhopper.jar
 
+COPY docker/certs/* /usr/local/share/ca-certificates/
+RUN update-ca-certificates
+
+RUN keytool -import -noprompt -alias zscaler-corp-cert -trustcacerts -keystore ${JAVA_HOME}/lib/security/cacerts -storepass changeit -file /usr/local/share/ca-certificates/zscaler-root.pem.crt
+
 COPY docker/graphhopper-entrypoint.sh entrypoint.sh
 
 ENTRYPOINT /graphhopper/entrypoint.sh
