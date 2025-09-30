a8ce58ab5115c076f53cf28fb3bf84a3a49481dc
Martin Boos <mboos@outlook.com>
2025-09-30 06:41:42 +0200

    feat(geocode): improve geocoding

--- backend/core/management/commands/geocode.py
+++ backend/core/management/commands/geocode.py
@@ -1,5 +1,6 @@
 import djclick as click
 import httpx
+from django.contrib.gis.geos import Point
 from django.db import transaction
 
 from core.models import JobOpening
@@ -13,10 +14,19 @@ def command():
             o: JobOpening = queryset.select_for_update(skip_locked=True).first()
 
             print("geocoding: ", o)
+            parts_for_geocoding = [o.company_name, o.address, o.zip, o.city]
 
-            full_address = ", ".join([o.company_name, o.address, f"{o.zip} {o.city}"])
-            resp = httpx.get("http://localhost:2322/api/", params={"q": full_address}).raise_for_status()
-            data = resp.json()
-            print("response: ", data)
-            # todo: get and save location
-            break
+            while parts_for_geocoding:
+                full_address = ", ".join(filter(lambda p: len(p), parts_for_geocoding))
+                resp = httpx.get("http://localhost:2322/api/", params={"q": full_address}).raise_for_status()
+                data = resp.json()
+                features = data["features"]
+                if features:
+                    geom = features[0]["geometry"]
+                    o.location = Point(*geom["coordinates"], srid=4326)
+                    break
+                else:
+                    parts_for_geocoding = parts_for_geocoding[1:]
+            if not o.location:
+                o.location = Point(0, 0)
+            o.save()
